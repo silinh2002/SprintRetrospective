0ffa9e2ad3811cd5ad69f76646a2bcf31a350069
silinh2002 <silinh.200298@gmail.com>
2020-10-28 21:36:58 +0700

    second commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ConnectDB = require("./db");
-
+const cors = require("cors");
 const config = require("./config");
 
 const { port } = config;
@@ -10,7 +10,7 @@ const app = express();
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(cors());
 // parse application/json
 app.use(bodyParser.json());
 
--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 1000,
   mongoURL:
     process.env.MONGO_URL ||
-    "mongodb+srv://admin:lehoa2002@cluster0.apeq2.mongodb.net/SprintRetrospectiveDB?retryWrites=true&w=majority",
+    "mongodb+srv://admin:root@cluster0.apeq2.mongodb.net/SprintRetrospectiveDB?retryWrites=true&w=majority",
 };
